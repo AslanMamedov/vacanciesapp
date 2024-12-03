4fe3c4da559d84af03d65d2d135d7fa4d0154beb
Aslan Mammadov <a.a.mammdov@xalqsigorta.az>
2024-12-03 21:14:54 +0400

    added ant design and refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ` <router-outlet></router-outlet> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'vacanciesapp';
-}
+export class AppComponent {}
